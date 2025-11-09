88c1b74ef6e386f699d3c27a01c8bc5aa987ab3f
Philipp Galliker <philipp.galliker88@gmail.com>
2025-11-09 21:40:44 +0100

    Fix order of class names in header title for consistent styling (Prettier rule)

--- components/Header.tsx
+++ components/Header.tsx
@@ -25,7 +25,7 @@ const Header = () => {
             />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
-            <div className="hidden h-6 text-2xl font-semibold mb-1 sm:block">
+            <div className="mb-1 hidden h-6 text-2xl font-semibold sm:block">
               {siteMetadata.headerTitle}
             </div>
           ) : (
